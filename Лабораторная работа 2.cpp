8e5b3d2b3191a66d8610e565176d52f61940a17a
Валерий <73661678+ilonmask1488@users.noreply.github.com>
2024-03-10 18:46:12 +0300

    Add files via upload

---
+++ Лабораторная работа 2.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class TransportVehicle {
+private:
+	int Kolcol;
+	double mileage;
+	int Tank_capacity;
+	double Engine_power;
+	double Travel_time;
+	string Title;
+public:
+	TransportVehicle(int wheels, int mileage, int capacity, int power, string vehicleName):
+	Kolcol(wheels),mileage(mileage), Tank_capacity(capacity), Engine_power(power), Title(vehicleName)
+	{
+	}
+	//void Message() {
+	//	cout << "Введите данные" << endl;
+	//}
+	//void setDate(int wheels, int mileage, int capacity, int power, string vehicleName)
+	//{
+	//	Kolcol = wheels;
+	//	mileage = mileage;
+	//	Tank_capacity = capacity;
+	//	Engine_power = power;
+	//	Title = vehicleName;
+	//}
+	//void getDate() {
+	//	
+	//}
+
+
+	~TransportVehicle() {
+		cout << "Транспортное средство удалено" << endl;
+	}
+	
+
+
+};
